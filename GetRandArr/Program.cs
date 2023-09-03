1f8090a9ff2f3ba83371a8587af5993060e30770
PetrT <140140070+196-PetrT@users.noreply.github.com>
2023-09-03 19:06:32 +0300

    generate massive with random item [-9;9] and summ positive and negative item

---
+++ GetRandArr/Program.cs
@@ -0,0 +1,54 @@
+﻿// 31. Задайте массив из 12 элементов, заполненным случайными числами из промежутка [-9;9].
+// найдите сумму положительных и отрицательных элементов массива
+
+
+var array = GenerateArray(12); 
+
+PrintArray(array);
+SumPosit(array);
+SumNegat(array);
+
+
+int[] GenerateArray(int length)// метод создает ранд массив целого размера
+{
+    int[] result = new int[length];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(-9, 10);
+    }
+    return result;
+}
+
+void PrintArray(int[] array)// метод выводящий элементы массива
+{
+    foreach (int i in array)
+    {
+        Console.Write($"{i} ");
+    }
+}
+
+int SumPosit(int[] array) // метод считает сумму положительных значений 
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 0) sum += array[i];
+        
+    } 
+    Console.WriteLine();
+    Console.Write(sum);
+    return sum;
+}
+
+int SumNegat(int[] array) // метод считает сумму отрицательных значений 
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] <= 0) sum += array[i];
+        
+    } 
+    Console.WriteLine();
+    Console.Write(sum);
+    return sum;
+}
